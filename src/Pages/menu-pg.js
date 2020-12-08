ebeb05552b392b92bb60aaf73234d2ff4b50a74f
Kendra Kyndberg <kendrak922@gmail.com>
2020-12-08 16:27:15 -0600

    styles

--- src/Pages/menu-pg.js
+++ src/Pages/menu-pg.js
@@ -5,6 +5,11 @@ function Menu() {
   return (
     <div className="Menu">
       <header className="Menu-header">
+      <div className="transparent">
+     <h2> Let's Get Started Shall We?</h2>
+     <p>1. Answer Some Questions about Your Guests' Needs</p>
+     <p>2. Browse </p>
+     </div>
       </header>
       <FoodOptions />
     </div>
